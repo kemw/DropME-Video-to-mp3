5faecd7acdf6f08ada56a920dcc7d4220a6340c5
KALIIX <kamilnasser9@gmail.com>
2021-06-11 13:37:25 -0500

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace DropME_Video_to_mp3
+{
+    partial class DropMe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DropMe));
+            this.Dir = new System.Windows.Forms.Label();
+            this.DirLoc = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fileInput = new System.Windows.Forms.TextBox();
+            this.done = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Videobox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Videobox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Dir
+            // 
+            this.Dir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dir.Location = new System.Drawing.Point(8, 256);
+            this.Dir.Name = "Dir";
+            this.Dir.Size = new System.Drawing.Size(406, 29);
+            this.Dir.TabIndex = 1;
+            // 
+            // DirLoc
+            // 
+            this.DirLoc.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.DirLoc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DirLoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DirLoc.Location = new System.Drawing.Point(155, 288);
+            this.DirLoc.Name = "DirLoc";
+            this.DirLoc.Size = new System.Drawing.Size(99, 29);
+            this.DirLoc.TabIndex = 3;
+            this.DirLoc.Text = "Convert";
+            this.DirLoc.UseVisualStyleBackColor = false;
+            this.DirLoc.Click += new System.EventHandler(this.DirLoc_Click);
+            // 
+            // fileInput
+            // 
+            this.fileInput.AllowDrop = true;
+            this.fileInput.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.fileInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileInput.Location = new System.Drawing.Point(12, 214);
+            this.fileInput.Name = "fileInput";
+            this.fileInput.Size = new System.Drawing.Size(391, 22);
+            this.fileInput.TabIndex = 5;
+            // 
+            // done
+            // 
+            this.done.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.done.Location = new System.Drawing.Point(12, 175);
+            this.done.Name = "done";
+            this.done.Size = new System.Drawing.Size(391, 23);
+            this.done.Step = 2;
+            this.done.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Silver;
+            this.label2.Font = new System.Drawing.Font("AcmeFont", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Magenta;
+            this.label2.Location = new System.Drawing.Point(51, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(284, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Thank you for using This Software";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(314, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Help";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Firebrick;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Indigo;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 33);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(168, 17);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Click  here to see your mp3";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.LightGreen;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Videobox
+            // 
+            this.Videobox.ErrorImage = ((System.Drawing.Image)(resources.GetObject("Videobox.ErrorImage")));
+            this.Videobox.Image = global::DropME_Video_to_mp3.Properties.Resources.home5;
+            this.Videobox.Location = new System.Drawing.Point(12, 66);
+            this.Videobox.Name = "Videobox";
+            this.Videobox.Size = new System.Drawing.Size(382, 103);
+            this.Videobox.TabIndex = 0;
+            this.Videobox.TabStop = false;
+            this.Videobox.Click += new System.EventHandler(this.Videobox_Click);
+            this.Videobox.DragEnter += new System.Windows.Forms.DragEventHandler(this.Videobox_DragEnter);
+            // 
+            // DropMe
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 329);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.done);
+            this.Controls.Add(this.fileInput);
+            this.Controls.Add(this.DirLoc);
+            this.Controls.Add(this.Dir);
+            this.Controls.Add(this.Videobox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "DropMe";
+            this.Text = "DropME-Video to mp3";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DropMe_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.Videobox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Videobox;
+        private System.Windows.Forms.Label Dir;
+        private System.Windows.Forms.Button DirLoc;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox fileInput;
+        private System.Windows.Forms.ProgressBar done;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿
+/*
+* Build and design by kemw tech 
+* This software is free to use any one to Learn C# Programing
+* Version 1.0
+*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace DropME_Video_to_mp3
+{
+    public partial class DropMe : Form
+    {
+
+      //  private String[] filepath;
+        string file_path;
+        public DropMe()
+        {
+            InitializeComponent();
+            ((Control)Videobox).AllowDrop = true;
+         
+
+        }
+
+      
+
+        
+       
+
+     
+
+       
+
+        private void DirLoc_Click(object sender, EventArgs e)
+        {
+            Transcode();
+        }
+
+        private void Transcode()
+        {
+            try
+            {
+                timer1.Interval = openFileDialog1.SafeFileName.Length;
+                timer1.Start();
+                file_path = openFileDialog1.FileName;
+                string name = Path.GetFileNameWithoutExtension(file_path);
+                string command = string.Format("-i {0} {1}.mp3", file_path, name);
+                Console.WriteLine("comm" + command);
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    //  WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = Application.StartupPath + "\\ffmpeg.exe",
+                    Arguments = command
+                };
+               
+                process.StartInfo = startInfo;
+                process.Start();
+                if (process.HasExited)
+                {
+                    MessageBox.Show("finish");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Eror while converting file " + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            done.Increment(1);
+            if (done.Value == done.Maximum)
+            {
+                timer1.Stop();
+                string file_path = fileInput.Text;
+                string name = Path.GetFileNameWithoutExtension(file_path) + ".mp3";
+                MessageBox.Show("Done converting " + file_path + "\nNew File created " + name, "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                done.Value = done.Minimum;
+                label2.Show();
+            }
+        }
+
+        private void Videobox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.ShowDialog();
+                string path = openFileDialog1.FileName;
+                fileInput.Text = path;
+
+                if (DialogResult == DialogResult.Cancel)
+                {
+                    fileInput.Text = "Select a file ...";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Eror while opening file (File must be an mp4 file!!)" + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (MessageBox.Show("If you need to Learn More About It...... Click Smash button to Github Repository...", "Help", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                Process.Start("https://github.com/kemw/DropME-Video-to-mp3");
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                string files_folder = Directory.CreateDirectory("mp3").FullName;
+
+                string folder = Environment.CurrentDirectory;
+
+
+                string[] items = System.IO.Directory.GetFiles(folder, "*.mp3", System.IO.SearchOption.TopDirectoryOnly);
+                foreach (string filePath in items)
+                {
+                    string newFile = System.IO.Path.Combine(files_folder, System.IO.Path.GetFileName(filePath));
+                    if (File.Exists(newFile))
+                    {
+                        MessageBox.Show("File already exists!!\n Overwriting!", "Exists", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        // File.Delete(file);
+                        File.Delete(filePath);
+                        continue;
+                    }
+
+                    File.Move(filePath, newFile);
+
+                }
+
+                string path = Directory.GetCurrentDirectory();
+                string folder1 = "mp3";
+                string full_path = Path.Combine(path, folder1);
+
+                Process.Start("explorer.exe", full_path);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eror while moving files " + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+
+        private void DropMe_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (Process processs in Process.GetProcessesByName(Application.StartupPath + "\\ffmpeg.exe"))
+            {
+                processs.Kill();
+            }
+            foreach (Process processs in Process.GetProcessesByName("DropME-Video-to-mp3.exe"))
+            {
+                processs.Kill();
+
+            }
+            Environment.Exit(0);
+            Application.Exit();
+            Dispose();
+        }
+
+        private void Videobox_DragEnter(object sender, DragEventArgs e)
+        {
+            file_path = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            fileInput.Text=file_path;
+            
+           // Transcode();
+        }
+
+       
+
+       
+        
+
+        
+
+      
+        
+        
+
+        
+    }
+}
